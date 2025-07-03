af8e234f9825391912147dc4db946ab12b03deb0
haniyeh amiri <haniyehamirilol@gmail.com>
2025-07-03 19:07:13 +0330

    feat: create  ProductFormInfo and add it in AddProductForm

--- src/features/addProduct/AddProductForm.tsx
+++ src/features/addProduct/AddProductForm.tsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import { PickerColor } from "./PickerColor";
+import { ProductInfoBox } from "./ProductInfoBox";
 
 export const AddProductForm = () => {
   const [colorSelect, setColorSelect] = useState<string[]>([]);
   return (
     <div className="mt-10">
-      <form className="flex items-center justify-center">
+      <form className="flex items-center justify-center sm:flex-row flex-col gap-5">
+        <ProductInfoBox />
         <PickerColor
           colorSelect={colorSelect}
           setColorSelect={setColorSelect}
---
+++ src/features/addProduct/ProductInfoBox.tsx
@@ -0,0 +1,67 @@
+import { useRef, useState } from "react";
+
+import { useDisplay } from "../../hooks/useDisplay";
+import { useClickOutside } from "../../hooks/useClickOutside";
+
+import { BlackBox, Input } from "../../ui/StyleComponents";
+import { Colorful } from "./Colorful";
+import { ColorBox } from "./ColorBox";
+import { useProductForm } from "../../contexts/ProductFormProvider";
+
+export const ProductInfoBox = () => {
+  const [mainColor, setMainColor] = useState("#333");
+  const { setIsOpen, isOpen } = useDisplay();
+  const pickerColorRef = useRef<HTMLDivElement | null>(null);
+  const { register } = useProductForm();
+
+  // close color picker when clicking outside
+  useClickOutside(pickerColorRef, () => setIsOpen(false));
+
+  return (
+    <BlackBox className="grid grid-cols-2 gap-2 w-[90%] min-[400px]:w-[17rem] md:w-[22rem]">
+      <Input
+        type="text"
+        placeholder="name"
+        {...register("name", { required: "please fill it" })}
+      />
+      <Input
+        type="text"
+        placeholder="brand"
+        {...register("brand", { required: "please fill it" })}
+      />
+      <Input
+        type="text"
+        placeholder="price"
+        {...register("price", { required: "please fill it" })}
+      />
+      <Input
+        type="text"
+        placeholder="discount"
+        {...register("discount", { required: "please fill it" })}
+      />
+      <div className="relative">
+        <ColorBox color={mainColor} title="main color" isOpen={isOpen}>
+          {!isOpen && (
+            <button
+              type="button"
+              className="mt-1"
+              onClick={() => setIsOpen(true)}
+            >
+              <span
+                style={{ background: mainColor }}
+                className="inline-block rounded-full w-6 h-6"
+              ></span>
+            </button>
+          )}
+        </ColorBox>
+        {isOpen && (
+          <Colorful
+            color={mainColor}
+            setColor={setMainColor}
+            pickerColorRef={pickerColorRef}
+          />
+        )}
+      </div>
+    </BlackBox>
+  );
+};

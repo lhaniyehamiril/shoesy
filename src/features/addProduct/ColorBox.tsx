94dff0e4fabf3c4e40d13b8958eb96572b7ca947
haniyeh amiri <haniyehamirilol@gmail.com>
2025-07-03 18:51:32 +0330

    feat: add reusable ColorBox component

---
+++ src/features/addProduct/ColorBox.tsx
@@ -0,0 +1,30 @@
+import { ReactNode } from "react";
+
+type colorBoxProps = {
+  title: string;
+  color: string;
+  isOpen: boolean | null;
+  children: ReactNode;
+};
+
+export const ColorBox: React.FC<colorBoxProps> = ({
+  title,
+  isOpen,
+  color,
+  children,
+}) => {
+  return (
+    <div className="flex items-center gap-2 bg-[var(--color-purple)] w-full h-13 px-3 rounded-full  ">
+      <span>{title} : </span>
+      {children}
+      {isOpen && (
+        <button type="button" className="mt-1">
+          <span
+            style={{ background: color }}
+            className="inline-block rounded-full w-6 h-6"
+          ></span>
+        </button>
+      )}
+    </div>
+  );
+};

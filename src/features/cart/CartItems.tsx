3a6be698f75061bf4e50d23def06fb9a08840376
haniyeh amiri <haniyehamirilol@gmail.com>
2025-08-03 18:29:08 +0330

    refctor: add iconTrash component

--- src/features/cart/CartItems.tsx
+++ src/features/cart/CartItems.tsx
@@ -3,6 +3,7 @@ import { dataCart } from "./CartSlice";
 import { ImgWithLoader } from "../../ui/ImgWithLoader";
 import { Quantity } from "../productsDetails/Quantity";
 import { Discount } from "../../ui/Discount";
+import { IconTrash } from "../../icons/IconTrash";
 
 type CartItemsProps = {
   items: dataCart;
@@ -18,7 +19,6 @@ export const CartItems: React.FC<CartItemsProps> = ({ items, onRemove }) => {
             src={items.image}
             alt={items.name}
             className="w-16 h-20 shrink-0"
-            borderColor="border-[var(--color-gray-primary)]"
           />
         </div>
       </div>
@@ -41,42 +41,7 @@ export const CartItems: React.FC<CartItemsProps> = ({ items, onRemove }) => {
             onClick={() => onRemove(items.id)}
             className=" bg-[var(--color-purple)] px-1 rounded-full flex items-center justify-center cursor-pointer transition hover:bg-[#c387fff3]"
           >
-            <svg
-              width="20px"
-              height="20px"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g id="Iconly/Curved/Delete">
-                <g id="Delete">
-                  <path
-                    id="Stroke 1"
-                    d="M18.8892 9.5542C18.8892 17.5732 20.0435 21.198 12.2797 21.198C4.5149 21.198 5.693 17.5732 5.693 9.5542"
-                    stroke="#333"
-                    stroke-width="1.5"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                  <path
-                    id="Stroke 3"
-                    d="M20.3651 6.47985H4.2146"
-                    stroke="#333"
-                    stroke-width="1.5"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                  <path
-                    id="Stroke 5"
-                    d="M15.7148 6.47983C15.7148 6.47983 16.2434 2.71411 12.2891 2.71411C8.33578 2.71411 8.86435 6.47983 8.86435 6.47983"
-                    stroke="#333"
-                    stroke-width="1.5"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </g>
-              </g>
-            </svg>
+            <IconTrash strokeColor="#333" />
           </button>
         </div>
       </div>

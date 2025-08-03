39c187f360d198505849d8ecb19c4a53f6a7dd63
haniyeh amiri <haniyehamirilol@gmail.com>
2025-08-03 18:47:29 +0330

    feat: add product add link only for user haniyhe

--- src/features/navbar/NavApp.tsx
+++ src/features/navbar/NavApp.tsx
@@ -16,7 +16,13 @@ export const NavApp = () => {
   return (
     <div className="z-[999] w-full md:w-28 flex items-center justify-center md:justify-start md:items-center md:h-0 md:fixed md:top-52 md:ml-5">
       <nav className="bg-white flex items-center justify-center fixed z-[999] bottom-0 md:static py-3 md:py-10 w-full">
-        <ul className="flex items-center justify-center px-32 md:px-1 gap-6 md:flex-col md:gap-5 w-[75%] min-[438px]:w-32 py-3 md:w-16 md:py-7 bg-[var(--color-gray-primary)] rounded-full">
+        <ul
+          className={`flex items-center justify-center px-32 md:px-1 gap-6 md:flex-col md:gap-5 ${
+            username
+              ? "w-[80%] max-[358px]:w-[90%] min-[438px]:w-[20rem]"
+              : " w-[75%] min-[438px]:w-32"
+          }  py-3 md:w-16 md:py-7 bg-[var(--color-gray-primary)] rounded-full`}
+        >
           <li>
             <NavItem to="/" currentPath={location.pathname} icon={<House />} />
           </li>
@@ -34,13 +40,15 @@ export const NavApp = () => {
               icon={<IconCart width="28" height="28" />}
             />
           </li>
-          <li>
-            <NavItem
-              to="/product/add"
-              currentPath={location.pathname}
-              icon={<IconAdd />}
-            />
-          </li>
+          {username === "haniyeh" && (
+            <li>
+              <NavItem
+                to="/product/add"
+                currentPath={location.pathname}
+                icon={<IconAdd />}
+              />
+            </li>
+          )}
           {username && (
             <li className="mt-2">
               <Logout />

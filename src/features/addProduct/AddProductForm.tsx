60c5a34f9336c2ed88b371b7472d8a1dce99089a
haniyeh amiri <haniyehamirilol@gmail.com>
2025-08-03 18:19:10 +0330

    feat(AddProductForm): add mainColor, isSubmit state, description component, and submit button

--- src/features/addProduct/AddProductForm.tsx
+++ src/features/addProduct/AddProductForm.tsx
@@ -1,11 +1,24 @@
 import { useState } from "react";
+import { motion } from "motion/react";
+
+import { useProductForm } from "../../contexts/ProductFormProvider";
+
+import { dataShoes } from "../../services/type";
+import { imagesType } from "./types";
+
 import { PickerColor } from "./PickerColor";
 import { ProductInfoBox } from "./ProductInfoBox";
 import { ProductImgBox } from "./ProductImgBox";
-import { imagesType } from "./types";
 import { BoxSize } from "./BoxSize";
+import { usePostData } from "./usePostData";
+import { Description } from "./Description";
+
+import { variantX } from "../../ui/variantMotion";
+import { IconProduct } from "../../icons/IconProduct";
+import { Spinner } from "../../ui/Spinner";
 
 export const AddProductForm = () => {
+  const [mainColor, setMainColor] = useState("#333");
   const [colorSelect, setColorSelect] = useState<string[]>([]);
   const [sizeSelect, setSizeSelect] = useState<number[]>([]);
   const [images, setImages] = useState<imagesType>({
@@ -13,23 +26,92 @@ export const AddProductForm = () => {
     under: "",
     top: "",
   });
+  const [isSubmit, setIsSubmit] = useState(false);
+  const { handleSubmit, reset } = useProductForm();
+  const { mutate, isPending } = usePostData();
+
+  const onSubmit = (data: dataShoes) => {
+    const generateId = Date.now() * 1000 + Math.floor(Math.random() * 1000);
+
+    const newProduct: dataShoes = {
+      id: generateId,
+      brand: data.brand,
+      name: data.name,
+      images: images,
+      description: data.description,
+      sizes: sizeSelect,
+      color: colorSelect,
+      mainColor: mainColor,
+      price: data.price,
+      discount: data.discount || 0,
+    };
+    mutate(newProduct);
+    setMainColor("#333");
+    setColorSelect([]);
+    setSizeSelect([]);
+    setImages({ main: "", under: "", top: "" });
+    reset();
+    setIsSubmit(false);
+  };
+
+  const handleButtonClick = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    setIsSubmit(true);
+    handleSubmit(onSubmit)();
+  };
+
   return (
-    <div className="mt-10 flex items-center justify-center sm:flex-row flex-col gap-5">
-      <div className="flex flex-col gap-2 sm:translate-y-9 items-center justify-center md:items-end">
-        <ProductImgBox setImages={setImages} images={images} />
-        <form className="flex items-center justify-center">
-          <ProductInfoBox />
-        </form>
-      </div>
+    <>
+      <div className="flex items-center justify-center mt-8 min-[680px]:mt-15 md:mt-4 sm:flex-row flex-col gap-5 sm:gap-4">
+        <div className="flex flex-col gap-2  items-center justify-center md:items-end">
+          <ProductImgBox
+            setImages={setImages}
+            images={images}
+            isSubmit={isSubmit}
+          />
+          <form
+            onSubmit={(e) => e.preventDefault()}
+            className="flex items-center justify-center flex-col gap-4"
+          >
+            <ProductInfoBox mainColor={mainColor} setMainColor={setMainColor} />
+            <Description />
+          </form>
+        </div>
 
-      <div className="flex justify-center flex-col gap-3 items-center max-[400px]:w-full">
-        <PickerColor
-          colorSelect={colorSelect}
-          setColorSelect={setColorSelect}
-        />
+        <div className="flex justify-center flex-col gap-3 items-center max-[400px]:w-full">
+          <PickerColor
+            colorSelect={colorSelect}
+            setColorSelect={setColorSelect}
+          />
 
-        <BoxSize sizeSelect={sizeSelect} setSizeSelect={setSizeSelect} />
+          <BoxSize
+            sizeSelect={sizeSelect}
+            setSizeSelect={setSizeSelect}
+            isSubmit={isSubmit}
+          />
+        </div>
       </div>
-    </div>
+      <div className="flex items-center justify-center mt-10">
+        <motion.button
+          variants={variantX}
+          initial="hidden"
+          animate="visible"
+          transition={{ delay: 0.6 }}
+          type="button"
+          onClick={handleButtonClick}
+          className="w-32 h-13 cursor-pointer absolute mt-3 md:mt-0 md:bottom-10 sm:right-15 font-bold flex items-center justify-center bg-[var(--color-purple)] rounded-full gap-1"
+        >
+          {isPending ? (
+            <Spinner />
+          ) : (
+            <>
+              Add <IconProduct />
+            </>
+          )}
+        </motion.button>
+      </div>
+    </>
   );
 };
---
+++ src/features/addProduct/Description.tsx
@@ -0,0 +1,34 @@
+import { motion } from "motion/react";
+
+import { useProductForm } from "../../contexts/ProductFormProvider";
+
+import { IconDescription } from "../../icons/IconDescription";
+import { BlackBox } from "../../ui/StyleComponents";
+import { variantX } from "../../ui/variantMotion";
+
+export const Description = () => {
+  const { register, errors } = useProductForm();
+  return (
+    <motion.div
+      variants={variantX}
+      initial="hidden"
+      animate="visible"
+      transition={{ delay: 0.3 }}
+      className="w-full flex items-center justify-center"
+    >
+      <BlackBox className="w-[88%] min-[400px]:w-[22rem] min-[640px]:w-[17rem] flex  p-4 flex-col gap-3">
+        <IconDescription />
+        <h2 className="text-center -mt-8 text-white">Description</h2>
+        <textarea
+          className={` rounded-[1.2rem] ${
+            errors.description?.message
+              ? "bg-[#ff7777]"
+              : "bg-[var(--color-purple)]"
+          } leading-6 w-full p-3 focus:outline-none`}
+          {...register("description", { required: "should be fill" })}
+          placeholder="Type"
+        />
+      </BlackBox>
+    </motion.div>
+  );
+};

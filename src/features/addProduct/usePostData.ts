149aa792983cf875ffea074365a183f41e45ae58
haniyeh amiri <haniyehamirilol@gmail.com>
2025-08-03 15:11:42 +0330

    feat(hooks): add usePostData hook with useMutation for API data posting

---
+++ src/features/addProduct/usePostData.ts
@@ -0,0 +1,10 @@
+import { useMutation } from "@tanstack/react-query";
+import { postDataShoes } from "../../services/postDataShoes";
+
+export const usePostData = () => {
+  const { mutate, isPending } = useMutation({
+    mutationFn: postDataShoes,
+  });
+
+  return { mutate, isPending };
+};

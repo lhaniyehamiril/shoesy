68fb77832d984718c5e1de23fbd26900c17bcde2
haniyeh amiri <haniyehamirilol@gmail.com>
2025-08-17 23:56:26 +0330

    refactor(Footer): resize footer

--- src/ui/Footer.tsx
+++ src/ui/Footer.tsx
@@ -3,7 +3,7 @@ import { Logo } from "./Logo";
 export const Footer = () => {
   return (
     <div className=" flex items-center justify-center ">
-      <div className="max-[352px]:text-[12px] text-[15px] w-[93%] sm:w-[30rem] md:mr-15 font-bold p-[1.3rem] pb-2 pt-5 absolute bottom-0 text-white bg-[var(--color-gray-primary)] rounded-t-4xl  items-center flex justify-between">
+      <div className="max-[352px]:text-[12px] text-[15px] w-[93%] sm:w-[30rem] md:mr-15 font-bold p-[1.3rem] pb-3 absolute bottom-0 text-white bg-[var(--color-gray-primary)] rounded-t-4xl  items-center flex justify-between">
         <span>
           build by{" "}
           <a

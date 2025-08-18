be3f11c69957d17bad3d6cc99aaae6a46eebf224
haniyeh amiri <haniyehamirilol@gmail.com>
2025-08-18 13:28:36 +0330

    refactor(Footer): remove Logo and add git icon

--- src/ui/Footer.tsx
+++ src/ui/Footer.tsx
@@ -1,19 +1,26 @@
-import { Logo } from "./Logo";
-
 export const Footer = () => {
   return (
-    <div className=" flex items-center justify-center ">
-      <div className="max-[352px]:text-[12px] text-[15px] w-[93%] sm:w-[30rem] md:mr-15 font-bold p-[1.3rem] pb-3 absolute bottom-0 text-white bg-[var(--color-gray-primary)] rounded-t-4xl  items-center flex justify-between">
+    <div className="pl-3">
+      <div className="flex items-center gap-2 rounded-full max-[352px]:text-[12px] bg-[var(--color-gray-primary)] text-white text-[15px] font-bold p-[1rem] px-4 absolute bottom-5">
         <span>
-          build by{" "}
-          <a
-            href="https://github.com/lhaniyehamiril"
-            className="text-[var(--color-purple)] underline"
-          >
-            haniyeh amiri
-          </a>
+          build by <b className="text-[var(--color-purple)]">haniyeh</b>
         </span>
-        <Logo />
+        <a href="https://github.com/lhaniyehamiril/shoesy">
+          <svg
+            width="25px"
+            height="25px"
+            viewBox="0 0 16 16"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+          >
+            <path
+              fill="#fff"
+              fill-rule="evenodd"
+              d="M8 1C4.133 1 1 4.13 1 7.993c0 3.09 2.006 5.71 4.787 6.635.35.064.478-.152.478-.337 0-.166-.006-.606-.01-1.19-1.947.423-2.357-.937-2.357-.937-.319-.808-.778-1.023-.778-1.023-.635-.434.048-.425.048-.425.703.05 1.073.72 1.073.72.624 1.07 1.638.76 2.037.582.063-.452.244-.76.444-.935-1.554-.176-3.188-.776-3.188-3.456 0-.763.273-1.388.72-1.876-.072-.177-.312-.888.07-1.85 0 0 .586-.189 1.924.716A6.711 6.711 0 018 4.381c.595.003 1.194.08 1.753.236 1.336-.905 1.923-.717 1.923-.717.382.963.142 1.674.07 1.85.448.49.72 1.114.72 1.877 0 2.686-1.638 3.278-3.197 3.45.251.216.475.643.475 1.296 0 .934-.009 1.688-.009 1.918 0 .187.127.404.482.336A6.996 6.996 0 0015 7.993 6.997 6.997 0 008 1z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </a>
       </div>
     </div>
   );
